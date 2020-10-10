1b19780fce2f9c516f46537a581ff2055e40e56c
hamid2117 <saim008865@gmail.com>
2020-10-10 20:48:54 +0500

    Updates

---
+++ precache-manifest.e6106f9168be4d5ce9845b77cd086bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c488734154cf90d55fb4d8d2eb2b74b2",
+    "url": "/First-react-app-/index.html"
+  },
+  {
+    "revision": "19be7c6b518187dfd110",
+    "url": "/First-react-app-/static/css/main.9ef4014b.chunk.css"
+  },
+  {
+    "revision": "4017af2ea18ab9472c6f",
+    "url": "/First-react-app-/static/js/2.7f343bcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-react-app-/static/js/2.7f343bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19be7c6b518187dfd110",
+    "url": "/First-react-app-/static/js/main.62994d3d.chunk.js"
+  },
+  {
+    "revision": "56ccb1c251bc5f8fd413",
+    "url": "/First-react-app-/static/js/runtime-main.a1666a05.js"
+  }
+]);
